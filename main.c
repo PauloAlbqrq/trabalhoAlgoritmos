b162d2fc88591c4597180632b2be70433df80d31
Matheus Henrique <115172554+Modheus@users.noreply.github.com>
2025-02-06 13:17:22 -0400

    Add files via upload
    Culpa da UI/UX podre do github desktop.

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define TMN_MAXIMO 3
 
@@ -15,12 +24,14 @@ void inicializarPilha(pilha* stack){
 }
 
 void empilhar(pilha* stack, const char* item){
-  if (stack->apontador < TMN_MAXIMO - 1) {
+  if (stack->apontador < TMN_MAXIMO) {
     stack->apontador++;
     strncpy(stack->pilhaArray[stack->apontador], item, 50);
     stack->pilhaArray[stack->apontador][49] = '\0';
     stack->tamanho++;
+    return;
   }
+  
 }
 
 char* desempilhar(pilha* stack){
@@ -39,18 +50,44 @@ void exibirHistorico(pilha* stack){
   }
 }
 
+void menu(pilha* stack) {
+    bool loop = true; // variável de controle do loop
+    while (loop) {
+        int num;
+        printf("Escolha uma opção: \n 1 - Pesquisar \n 2 - Voltar \n 3 - Ver historico \n 4 - Sair \n");
+        // loop que repete até que a função scanf retorne que a atribuição da variável foi um sucesso
+        while (scanf("%d", &num) < 1) { 
+            printf("Escolha uma opção: \n 1 - Pesquisar \n 2 - Voltar \n 3 - Ver historico \n 4 - Sair \n");
+            while (getchar() != '\n'); // Isso limpa o buffer ou o input, mas não entendi bem como
+        }
+        // usa um switch para não ficar escrevendo if e else if
+        switch(num) {
+            case 1:
+                char input[100]; // isso aqui deu problema por eu tentar usar um ponteiro pra um char sem memória alocada, então usei um array pra armazenar a string de caracteres
+                printf("Escreva o que deseja Pesquisar: ");
+                scanf(" %[^\n]", input); // não me pergunte. Só sei que o input não precisa do & por que toda variável de array já é um endereço de memória
+                empilhar(stack, input);
+                break;
+            case 2:
+                printf("%s \n", desempilhar(stack));
+                break;
+            case 3:
+                exibirHistorico(stack);
+                break;
+            case 4:
+                loop = false;
+                break;
+           
+        }
+    }
+}
+
 int main(void) {
+    
   pilha historico;
   inicializarPilha(&historico);
 
-  empilhar(&historico, "www.google.com");
-  empilhar(&historico, "www.youtube.com");
-  empilhar(&historico, "www.facebook.com");
-
-  desempilhar(&historico);
-
-  exibirHistorico(&historico);
+  menu(&historico);
 
   return 0;
 }
-
